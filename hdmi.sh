362199948b5e2bc0e4d3bb5f4acf34b7aaec879e
annadorottya <simon.anna.d@gmail.com>
2020-04-02 20:51:12 +0200

    VLC goes to TV + swiping

--- hdmi.sh
+++ hdmi.sh
@@ -2,10 +2,10 @@
 
 if xrandr -q | grep -q "HDMI1 connected"; then
 	echo "HDMI connected, activating"
-	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
+	xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal
 	pactl set-card-profile 0 output:hdmi-stereo-extra1
 else
 	echo "HDMI not connected, deactivating"
-	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
+	xrandr --output HDMI1 --off
 	pactl set-card-profile 0 output:analog-stereo
 fi
